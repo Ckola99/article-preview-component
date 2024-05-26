8e8aea5c7fe37d27bb043d3d33ac94d059bea298
Ckola99 <Christopherkola@gmail.com>
2024-05-26 21:07:09 +0200

    desktop version

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-const shareButton = document.querySelector("#button")
-const closeButton = document.querySelector("#button-close")
-const shareSection = document.querySelector('.share-section')
-const profileSection = document.querySelector('.profile')
+const shareButton = document.querySelector("#button");
+const closeButton = document.querySelector("#button-close");
+const shareSection = document.querySelector(".share-section");
+const profileSection = document.querySelector(".profile");
+const triangle = document.querySelector(".triangle");
 
 shareButton.addEventListener("click", () => {
-	shareSection.classList.remove('hidden');
-	profileSection.classList.add('hidden');
-	shareSection.style.display = 'flex';
+    if (window.innerWidth >= 600) {
+        triangle.classList.remove('hidden');
+        closeButton.classList.remove('hidden');
+        shareSection.style.display = 'flex';
+    } else {
+        shareSection.classList.remove('hidden');
+        profileSection.classList.add('hidden');
+        shareSection.style.display = 'flex';
+    }
 });
 
 closeButton.addEventListener("click", () => {
-	shareSection.classList.add('hidden');
-	profileSection.classList.remove('hidden');
-	shareSection.style.display = 'none';
+    if (window.innerWidth >= 600) {
+        shareSection.classList.add('hidden');
+        triangle.classList.add('hidden');
+        closeButton.classList.add('hidden');
+        shareSection.style.display = 'none';
+    } else {
+        shareSection.classList.add('hidden');
+        profileSection.classList.remove('hidden');
+        shareSection.style.display = 'none';
+    }
 });
